cc25a38fb04159207173bf3d31466234bf3f1335
Your Name <you@example.com>
2024-05-11 17:36:13 +0530

    added icons file

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from './icon_assets/Logo.svg'
+
+function Nav() {
+    return(
+        <nav>
+            {/*<img src={logo}  alt=""/>*/}
+        </nav>
+    )
+}
+
+export default Nav;
