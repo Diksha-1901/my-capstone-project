1339a9d8448c75ecf3682725914d60ce327af52b
Your Name <you@example.com>
2024-05-15 14:37:38 +0530

    added many files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-
+import React from 'react';
 import './App.css';
-import Header from './Header';
-import Nav from './Nav';
-import Main from './Main';
-import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Nav/>
-      <Main/>
-      <Footer/>
+ 
+    <div>
+      <h1>Meta Frontend Developer Capstone Starter Files</h1>
     </div>
   );
 }
